990ff37a06a9d2629be8d4686a3877f430a0cdf5
Anthony Myhre <anthony.myhre@utexas.edu>
2016-11-03 00:42:10 -0500

    transferred js to objects rather than functions/variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,13 @@
 	if user's guess is correct, does not get subtracted from number of guesses
 */
 
-var words = ["CREEDBRATTON", "PHYLLISVANCE", "KEVINMALONE", "ANDYBERNARD", "DWIGHTSCHRUTE", "MICHAELSCOTT", "PAMHALPERT", "JIMHALPERT"];
+/*var words = ["CREEDBRATTON", "PHYLLISVANCE", "KEVINMALONE", "ANDYBERNARD", "DWIGHTSCHRUTE", "MICHAELSCOTT", "PAMHALPERT", "JIMHALPERT"];
 var sounds = ["assets/mp3/creed.m4a", "assets/mp3/phyllis.m4a", "assets/mp3/kevin.m4a", "assets/mp3/andy.m4a", "assets/mp3/dwight.m4a", "assets/mp3/michael.m4a", "assets/mp3/pam.m4a", "assets/mp3/jim.m4a"];
 var gifs = ["assets/images/creed.gif", "assets/images/phyllis.gif", "assets/images/kevin.gif", "assets/images/andy.gif", "assets/images/dwight.gif", "assets/images/michael.gif", "assets/images/pam.gif", "assets/images/jim.gif"]
 var winningSound;
 var winningGif;
 var currentWord;
 var displayWord;
-// var lastWord;
 var numOfGuesses;
 var guessedLetters;
 var wins = 0;
@@ -51,13 +50,13 @@ function initializeDisplayWords () {
 function checkGuess() {
 	// if the guess is correct, then display the guess
 	if ((currentWord.indexOf(guess) != -1) && (hiddenWord.indexOf(guess) == -1)) {
-		displayCorrectGuess();
-		updateDisplayWord();
+		displayCorrectGuess();*/
+/*		updateDisplayWord();
 		if (hiddenWord == currentWord) {
 			wins++;
 			winningSound.play();
 			document.getElementById('gif').innerHTML = "<img src=" + winningGif + ">";
-			lastWord = currentWord;
+			// lastWord = currentWord;
 			revealLastWord();
 			startGame();
 		}
@@ -96,7 +95,7 @@ function displayCorrectGuess() {
 
 function revealLastWord() {
 	displayWord = "";
-	for (i = 0; i < currentWord.length - 1; i++) {
+	for (var i = 0; i < currentWord.length - 1; i++) {
 		displayWord += currentWord.substr(i, 1) + " ";
 	}
 	displayWord += currentWord.substr(currentWord.length - 1);
@@ -105,7 +104,7 @@ function revealLastWord() {
 
 function updateDisplayWord() {
 	displayWord = hiddenWord.substr(0, 1);
-	for (i = 1; i < hiddenWord.length; i++) {
+	for (var i = 1; i < hiddenWord.length; i++) {
 		displayWord += " " + hiddenWord.substr(i, 1);
 	}
 }
@@ -130,4 +129,151 @@ document.onkeyup = function(event) {
 	document.getElementById('currentWord').innerHTML = "" + displayWord;
 	document.getElementById('numOfGuesses').innerHTML = "" + numOfGuesses;
 	document.getElementById('guessedLetters').innerHTML = " " + guessedLetters;
+}
+*/
+
+
+// Create the game object. This contains all of the properties (i.e., variables) and methods (i.e., functions)
+var game = {
+	// these are the properties
+	// this is an array of the possible words the user will guess
+	words: ["CREEDBRATTON", "PHYLLISVANCE", "KEVINMALONE", "ANDYBERNARD", "DWIGHTSCHRUTE", "MICHAELSCOTT", "PAMHALPERT", "JIMHALPERT"], 
+	/*
+		these are the sound clips that will play if the user correctly guesses the word. each index is lined up with the indecies of words (same with gifs)
+		this ensures that the right clip will play when the word is correctly guess
+	*/
+	sounds: ["assets/mp3/creed.m4a", "assets/mp3/phyllis.m4a", "assets/mp3/kevin.m4a", "assets/mp3/andy.m4a", "assets/mp3/dwight.m4a", "assets/mp3/michael.m4a", "assets/mp3/pam.m4a", "assets/mp3/jim.m4a"],
+	// same idea as w/ the sounds, these are the gifs for each word
+	gifs: ["assets/images/creed.gif", "assets/images/phyllis.gif", "assets/images/kevin.gif", "assets/images/andy.gif", "assets/images/dwight.gif", "assets/images/michael.gif", "assets/images/pam.gif", "assets/images/jim.gif"],
+	// initialize properties that will be assigned later
+	winningSound: "",
+	winningGif: "",
+	currentWord: "",
+	displayWord: "",
+	hiddenWord: "",
+	guessedLetters: "",
+	numOfGuesses: "",
+	wins: 0,
+	// newPage lets the object know that the page has been reloaded 
+	newPage: true,
+	// plays when user inputs incorrect guess
+	idiot: new Audio('assets/mp3/dwight_idiot.mp3'),
+	// plays when user inputs correct guess
+	nailedIt: new Audio('assets/mp3/nailed_it.mp3'),
+	// plays when user uses all of its guesses
+	michael_scott: new Audio('assets/mp3/michael_scott.mp3'),
+	// plays when page loads
+	// openingSong: new Audio('assets/mp3/the_office.mp3'),
+	guess: "",
+
+	// this will start a new game
+	startGame: function () {
+		// generate random number to select a word to be guessed
+		var index = Math.floor(Math.random() * 8);
+		this.currentWord = this.words[index];
+		this.winningSound = new Audio(this.sounds[index]);
+		this.winningGif = this.gifs[index];
+		// clear string for guessed letters
+		this.guessedLetters = "";
+		// reset number of guesses
+		this.numOfGuesses = 12;
+		// call method to initialize display words
+		this.initializeDisplayWords();
+	},
+
+	// this method initilizes two words w/ _s, hiddenWord and displayWord
+	initializeDisplayWords: function () {
+		// this will be the same length as currentWord. It will be used to reveal guessed letters on displayWord later
+		this.hiddenWord = "";
+		/* 	this is used to display the user's correct guesses and the character places that have yet to be guessed
+			
+		*/
+		this.displayWord = "";
+		for ( var i = 0; i < this.currentWord.length - 1; i++ ) {
+			this.displayWord += "_ ";
+			this.hiddenWord += "_";
+		}
+		this.displayWord += "_";
+		this.hiddenWord += "_";
+	},
+
+	checkGuess: function () {
+		if ((this.currentWord.indexOf(this.guess) != -1) && (this.hiddenWord.indexOf(this.guess) == -1)) {
+			this.displayCorrectGuess();
+			this.updateDisplayWord();
+			if (this.hiddenWord == this.currentWord) {
+				this.wins++;
+				this.winningSound.play();
+				document.getElementById('gif').innerHTML = "<img src=" + this.winningGif + " />";
+				this.revealLastWord();
+				this.startGame();
+			}
+			else {
+				this.nailedIt.play();
+			}
+		}
+
+		else if ((this.guessedLetters.indexOf(this.guess) == -1) && (this.currentWord.indexOf(this.guess) == -1)) {
+			this.guessedLetters += this.guess + ", ";
+			this.numOfGuesses--;
+			if (this.numOfGuesses == 0) {
+				this.michael_scott.play();
+				this.revealLastWord();
+				this.startGame();
+			}
+
+			else {
+				this.idiot.play();
+			}
+		}
+	},
+
+	displayCorrectGuess: function () {
+		for ( var i = 0; i < this.currentWord.length; i++ ) {
+			if (this.currentWord[i] == this.guess) {
+				this.hiddenWord = this.hiddenWord.substr(0, i) + this.guess + this.hiddenWord.substr(i + 1);
+			}
+		}
+	},
+
+	revealLastWord: function () {
+		this.displayWord = "";
+		for ( var i = 0; i < this.currentWord.length - 1; i++ ) {
+			this.displayWord += this.currentWord.substr(i, 1) + " ";
+		}
+		this.displayWord += this.currentWord.substr(this.currentWord.length - 1);
+		document.getElementById('lastWord').innerHTML = "Last Word: " + this.displayWord;
+	},
+
+	updateDisplayWord: function () {
+		this.displayWord = this.hiddenWord.substr(0, 1);
+		for ( var i = 1; i < this.hiddenWord.length; i++ ) {
+			this.displayWord += " " + this.hiddenWord.substr(i, 1);
+		}
+	}
+}
+
+// game.openingSong.play();
+
+document.onkeyup = function(event) {
+	game.guess = String.fromCharCode(event.keyCode);
+	console.log(game);
+	if (game.newPage) {
+		game.newPage = false;
+		// game.openingSong.pause();
+		game.startGame();
+	}
+
+	else {
+		if (event.keyCode >= 65 && event.keyCode <= 90) {
+			game.checkGuess();
+		}
+	}
+
+	// display properties
+	document.getElementById('wins').innerHTML = "" + game.wins;
+	document.getElementById('currentWord').innerHTML = "" + game.displayWord;
+	document.getElementById('numOfGuesses').innerHTML = "" + game.numOfGuesses;
+	document.getElementById('guessedLetters').innerHTML = "" + game.guessedLetters;
+
 }
